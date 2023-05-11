3da9fd655dc145b69aeb6a074d38ac50fb8ed7d5
Violetta <94271862+burlakova-veta@users.noreply.github.com>
2023-05-11 20:46:38 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 0: Console.WriteLine("ноль"); break;
+                case 1: Console.WriteLine("один"); break;
+                case 2: Console.WriteLine("два"); break;
+                case 3: Console.WriteLine("три"); break;
+                case 4: Console.WriteLine("четыре"); break;
+                case 5: Console.WriteLine("пять"); break;
+                case 6: Console.WriteLine("шесть"); break;
+                case 7: Console.WriteLine("семь"); break;
+                case 8: Console.WriteLine("восемь"); break;
+                case 9: Console.WriteLine("девять"); break;
+            }
+        }
+    }
+}
